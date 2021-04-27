f886e293e1c47579b115986256214c23b50f158d
Carsten Lemmen <carsten.lemmen@hereon.de>
2021-04-27 15:43:22 +0200

    Added infrastructure code for VIMS models

---
+++ src/models/vims/vims_model_library.F90
@@ -0,0 +1,32 @@
+module vims_model_library
+
+   use fabm_types, only: type_base_model_factory, type_base_model
+
+   implicit none
+
+   private
+
+   type, extends(type_base_model_factory) :: type_factory
+   contains
+      procedure :: create
+   end type
+
+   type (type_factory), save, target, public :: vims_model_factory
+
+contains
+
+   subroutine create(self, name, model)
+
+      use vims_cosine ! Needs to be done for each vims model
+
+      class (type_factory), intent(in) :: self
+      character(*),         intent(in) :: name
+      class (type_base_model), pointer :: model
+
+      select case (name)
+         case ('cosine');   allocate(type_vims_cosine::model) ! Also add new models here
+      end select
+
+   end subroutine
+
+end module
