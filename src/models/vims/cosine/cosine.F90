b11070eedf8de92a15a4136f82960bf4e7da826b
Zhengui Wang <wzhengui@gmail.com>
2021-05-31 16:10:34 -0400

    add CoSiNE model to FABM framework

--- src/models/vims/cosine/cosine.F90
+++ src/models/vims/cosine/cosine.F90
@@ -46,16 +46,18 @@ module vims_cosine
       type (type_state_variable_id) :: id_DOX, id_CO2,  id_ALK
 
       !dependence
-      type (type_dependency_id) :: id_temp,id_salt,id_PAR
+      type (type_dependency_id) :: id_temp,id_salt,id_PAR,id_dep,id_zr
      
       !dianostic
       type (type_diagnostic_variable_id) :: id_PPR
 
       !model parameters
+      integer  :: idapt,ico2s,iz2graze
       real(rk) :: gmaxs1,gmaxs2,pis1,pis2,kno3s1,knh4s1,kpo4s1,kco2s1,kno3s2,knh4s2
-      real(rk) :: kpo4s2,kco2s2,ksio4s2,alpha1,alpha2,ak1,ak2,ak3,beta,gammas1,gammas2
+      real(rk) :: kpo4s2,kco2s2,ksio4s2,kns1,kns2,alpha1,alpha2,ak1,ak2,ak3,beta,gammas1,gammas2
       real(rk) :: beta1,beta2,kgz1,kgz2,rho1,rho2,rho3,gamma1,gamma2,gammaz,kex1,kex2,wss2,wsdn,wsdsi
       real(rk) :: si2n,p2n,o2no,o2nh,c2n,kox,kmdn1,kmdn2,kmdsi1,kmdsi2,gamman,TR,pco2a
+      real(rk) :: alpha_corr,zeptic,ndelay,rdelay
 
    contains
       procedure :: initialize
@@ -112,6 +114,9 @@ contains
       call self%get_parameter(self%kco2s2,'kco2s2','mmol m-3','half saturation of CO2 uptake by diatom',default=50.0_rk)
       call self%get_parameter(self%kco2s2,'ksio4s2','mmol m-3','half saturation of silicon uptake by diatom',default=4.5_rk)
 
+      call self%get_parameter(self%kns1,'kns1','none','nighttime NH4 update ratio for small phytoplankgon',default=0.0_rk)
+      call self%get_parameter(self%kns2,'kns2','none','nighttime NH4 update ratio for diatom',default=0.0_rk)
+
       call self%get_parameter(self%alpha1,'alpha1','W-1 m2 day-1','initial slope of P-I curve for small phytoplantkon',default=0.1_rk)
       call self%get_parameter(self%alpha2,'alpha2','W-1 m2 day-1','initial slope of P-I curve for diatom',default=0.1_rk)
       call self%get_parameter(self%beta,'beta','W-1 m2 day-1','photo-inhibition constant',default=0.0_rk)
@@ -158,6 +163,16 @@ contains
       call self%get_parameter(self%gamman,'gamman','day-1','nitrification coefficient',default=0.07_rk)
       call self%get_parameter(self%TR,'TR','oC','reference temperature for COSiNE kinetics',default=20.0_rk)
       call self%get_parameter(self%pco2a,'pco2a','ppm','atmospheric CO2 concentration',default=400.0_rk)
+
+      call self%get_parameter(self%iz2graze,'iz2graze','none','flag for mesozooplankton grazing',default=1)
+
+      call self%get_parameter(self%idapt,'idapt','none','flag for light adaption',default=0)
+      call self%get_parameter(self%alpha_corr,'alpha_corr','none','factor for light adaption',default=1.25_rk)
+      call self%get_parameter(self%zeptic,'zeptic','m','reference depth for light adaption',default=10.0_rk)
+
+      call self%get_parameter(self%ico2s,'ico2s','none','flag for CO2 limitation on phytoplankton growth',default=0)
+      call self%get_parameter(self%ndelay,'ndelay','day','number of days that mesozooplankton grazing is delayed',default=15.0_rk)
+      call self%get_parameter(self%rdelay,'rdelay','none','relavative contribution of concentration at ndelay_th day',default=0.3_rk)
    
       !--------------------------------------------------------
       ! Register the contribution of all state variables to total nitrogen
@@ -172,9 +187,11 @@ contains
       call self%register_diagnostic_variable(self%id_PPR,  'PPR', 'mmol m-3 d-1', 'gross primary production rate')
 
       ! Register environmental dependencies
-      call self%register_dependency(self%id_salt, standard_variables%practical_salinity)
       call self%register_dependency(self%id_temp, standard_variables%temperature)
-      call self%register_dependency(self%id_PAR, standard_variables%downwelling_photosynthetic_radiative_flux)
+      call self%register_dependency(self%id_salt, standard_variables%practical_salinity)
+      call self%register_dependency(self%id_zr,   standard_variables%depth)
+      call self%register_dependency(self%id_dep,  standard_variables%cell_thickness)
+      call self%register_dependency(self%id_PAR,  standard_variables%downwelling_photosynthetic_radiative_flux)
 
       ! Let phytoplankton (including background concentration) and detritus contribute to light attentuation
       call self%add_to_aggregate_variable(standard_variables%attenuation_coefficient_of_photosynthetic_radiative_flux, self%ak1)
@@ -191,12 +208,25 @@ contains
 
       real(rk), parameter :: secs_pr_day = 86400.0_rk
       real(rk) :: NO3,SiO4,NH4,S1,S2,Z1,Z2,DN,DSi,PO4,DOX,CO2,ALK
-      real(rk) :: PAR,temp
+      real(rk) :: temp,salt,zr,dep,PAR,qcos(13),mS2,mZ1,mDN,mZ2
+      real(rk) :: rtmp
+
+      !for precalculation
+      real(rk) :: fS1,fS2,bfNO3S1,bfNH4S1,bfNH4S2,bfNO3S2
+      real(rk) :: fNO3S1,fNH4S1,fNH4S2,fNO3S2,fPO4S1,fPO4S2,fCO2S1,fCO2S2,fSiO4S2
+      real(rk) :: pnh4s1,pnh4s2,pih1,pih2,rhot,rhop,ADPT,OXR,Tadjust
+    
+      !for kinetics
+      real(rk) :: NPS1,NPS2,RPS1,RPS2,SKS2,SKDN,SKDSi
+      real(rk) :: MTS1,MTS2,MTZ1,MTZ2,EXZ1,EXZ2
+      real(rk) :: GS1Z1,GS2Z2,GZ1Z2,GDNZ2,GTZ2
+      real(rk) :: Nit,MIDN,MIDSi
 
       ! Enter spatial loops (if any)
       _LOOP_BEGIN_
-
+         !-----------------------------------------------------------------
          ! Retrieve current (local) state variable values.
+         !-----------------------------------------------------------------
          _GET_(self%id_NO3, NO3)
          _GET_(self%id_SiO4,SiO4)
          _GET_(self%id_NH4, NH4)
@@ -212,24 +242,164 @@ contains
          _GET_(self%id_ALK, ALK)
 
          ! Retrieve current environmental conditions.
-          _GET_(self%id_PAR,PAR)          ! local photosynthetically active radiation
-          _GET_(self%id_temp,temp)          ! local photosynthetically active radiation
          ! _GET_SURFACE_(self%id_I_0,I_0)  ! surface photosynthetically active radiation
+          _GET_(self%id_PAR,PAR)          ! local photosynthetically active radiation
+          _GET_(self%id_temp,temp)      
+          _GET_(self%id_salt,salt)        
+          _GET_(self%id_zr,zr)        
+          _GET_(self%id_dep,dep)        
 
+         !-----------------------------------------------------------------
+         !CoSiNE model kinetics: pre-calculation
+         !-----------------------------------------------------------------
+         !temperature adjust          
+         Tadjust=exp(0.069_rk*(temp-self%TR))
+        
+         !Light limitation factor including photo-inhibition and light adaptation
+         ADPT=1.0_rk
+         if (self%idapt==1) ADPT=self%alpha_corr*(1.0_rk-4.0_rk*zr)/self%zeptic
+         pih1=(1.0_rk-exp(-PAR*ADPT*self%alpha1/self%gmaxs1))*exp(-self%beta*PAR/self%gmaxs1)
+         pih2=(1.0_rk-exp(-PAR*ADPT*self%alpha2/self%gmaxs2))*exp(-self%beta*PAR/self%gmaxs2)
+
+         !NH4 inhibition 
+         pnh4s1=min(1.0_rk,exp(-self%pis1*NH4))
+         pnh4s2=min(1.0_rk,exp(-self%pis2*NH4))
+    
+         !PO4, CO2 and SiO4 limitation factor
+         fPO4S1=PO4/(self%kpo4s1+PO4)
+         fCO2S1=CO2/(self%kco2s1+CO2)
+         fPO4S2=PO4/(self%kpo4s2+PO4)
+         fCO2S2=CO2/(self%kco2s2+CO2)
+         fSiO4S2=SiO4/(self%ksio4s2+SiO4)
+
+         !nitrogen limitation factor
+         rtmp=1.0_rk+NH4/self%knh4s1+pnh4s1*NO3/self%kno3s1
+         bfNO3S1=pnh4s1*NO3/(self%kno3s1*rtmp)
+         bfNH4S1=NH4/(self%knh4s1*rtmp)
+
+         rtmp=1.0_rk+NH4/self%knh4s2+pnh4s2*NO3/self%kno3s2
+         bfNO3S2=pnh4s2*NO3/(self%kno3s2*rtmp)
+         bfNH4S2=NH4/(self%knh4s2*rtmp)
+
+         !final limitation
+         if(self%ico2s==0) then
+           fS1=min(bfNO3S1+bfNH4S1,fPO4S1)
+           fS2=min(bfNO3S2+bfNH4S2,fSiO4S2,fPO4S2)
+         else
+           fS1=min(bfNO3S1+bfNH4S1,fPO4S1,fCO2S1)
+           fS2=min(bfNO3S2+bfNH4S2,fSiO4S2,fPO4S2,fCO2S2)
+         endif
+       
+         !adjustment for nitrogen limitation factors
+         fNO3S1=fS1*bfNO3S1/(bfNO3S1+bfNH4S1+1.0e-6)
+         fNH4S1=fS1*bfNH4S1/(bfNO3S1+bfNH4S1+1.0e-6)
+
+         fNO3S2=fS2*bfNO3S2/(bfNO3S2+bfNH4S2+1.0E-6)
+         fNH4S2=fS2*bfNH4S2/(bfNO3S2+bfNH4S2+1.0E-6)
+ 
+         !zooplankton grazing  
+         GS1Z1=self%beta1*Z1*S1/(self%kgz1+S1)
+         if(S1<=0.25_rk) GS1Z1=0.0_rk
+
+         mS2=S2; mZ1=Z1; mDN=DN; mZ2=Z2   !todo: need to add option for ndelay!=0
+         rhot=self%rho1*mS2+self%rho2*mZ1+self%rho3*mDN
+         rhop=self%rho1*mS2*mS2+self%rho2*mZ1*mZ1+self%rho3*mDN*mDN
+
+         GS2Z2=self%beta2*self%rho1*mS2*mS2*mZ2/(self%kgz2*rhot+rhop)
+         GZ1Z2=self%beta2*self%rho2*mZ1*mZ1*mZ2/(self%kgz2*rhot+rhop)
+         GDNZ2=self%beta2*self%rho3*mDN*mDN*mZ2/(self%kgz2*rhot+rhop)
+
+         !turn off mesozooplankton grazing at certain conditions
+         if((rhot<=0.0_rk .and. rhop<= 0.0_rk) .or. self%iz2graze==0) then
+           GS2Z2=0.0_rk; GZ1Z2=0.0_rk; GDNZ2=0.0_rk
+         endif
+         if (mS2<=0.5_rk) GS2Z2=0.0_rk
+         if (mZ1<=0.025_rk) GZ1Z2=0.0_rk
+
+         GTZ2=GS2Z2+GZ1Z2+GS2Z2
+
+         !oxidation 
+         OXR=DOX/(self%kox+DOX)
+
+         !-----------------------------------------------------------------
+         !CoSiNE model kinetics: computing the reaction rate 
+         !-----------------------------------------------------------------
+         !S1
+         NPS1=self%gmaxs1*fNO3S1*pih1*S1
+         RPS1=self%gmaxs1*max(self%kns1*NH4/(self%knh4s1+NH4),fNH4S1*pih1)*S1
+         MTS1=self%gammas1*S1
+         qcos(4)=NPS1+RPS1-GS1Z1-MTS1
+
+         !S2
+         NPS2=self%gmaxs2*fNO3S2*pih2*S2
+         RPS2=self%gmaxs2*max(self%kns2*NH4/(self%knh4s2+NH4),fNH4S2*pih2)*S2
+         MTS2=self%gammas2*S2
+         qcos(5)=NPS2+RPS2-GS2Z2-MTS2
+
+         !Z1
+         EXZ1=self%kex1*Z1
+         MTZ1=self%gammaz*Z1*Z1
+         qcos(6)=self%gamma1*GS1Z1-OXR*EXZ1-GZ1Z2-MTZ1
+        
+         !Z2
+         EXZ2=self%kex2*Z2 
+         MTZ2=self%gammaz*Z2*Z2
+         qcos(7)=self%gamma2*GTZ2-OXR*EXZ2-MTZ2
+
+         !DN
+         MIDN=max(self%kmdn1*temp+self%kmdn2,0.05_rk)*DN
+         qcos(8)=(1.0_rk-self%gamma1)*GS1Z1+(1.0_rk-self%gamma2)*GTZ2 &
+                &-GDNZ2+MTS1+MTS2+MTZ1+MTZ2-OXR*MIDN
+
+         !DSi
+         MIDSi=max(self%kmdsi1*temp+self%kmdsi2,0.01_rk)*DSi
+         qcos(9)=(GS2Z2+MTS2)*self%si2n-MIDSi
+
+         !NO3
+         Nit=self%gamman*NH4
+         qcos(1)=-NPS1-NPS2+OXR*Nit
+
+         !NH4
+         qcos(3)=-RPS1-RPS2+OXR*EXZ1+OXR*EXZ2-OXR*Nit+OXR*MIDN
+
+         !SiO4
+         qcos(2)=-(NPS2+RPS2)*self%si2n+MIDSi
+
+         !PO4
+         qcos(10)=-(NPS1+RPS1+NPS2+RPS2)*self%p2n+OXR*(EXZ1+EXZ2)*self%p2n &
+                &+OXR*MIDN*self%p2n+MIDSi*self%p2n/self%si2n 
+
+         !DOX       
+         qcos(11)=(NPS1+NPS2)*self%o2no+(RPS1+RPS2)*self%o2nh &
+                 &-2.0_rk*OXR*Nit-OXR*(EXZ1+EXZ2)*self%o2nh-OXR*MIDN*self%o2nh 
+
+         !CO2
+         qcos(12)=-(NPS1+RPS1+NPS2+RPS2)*self%c2n+OXR*(EXZ1+EXZ2)*self%c2n+OXR*MIDN*self%c2n
+
+         !ALK
+         qcos(13)=-qcos(1)+qcos(3)
+
+         !temperatue adjustl
+         qcos=qcos*Tadjust
+
+         !bottom and surface fluxes (todo)
+
+         !-----------------------------------------------------------------
          ! Set temporal derivatives
-         _ADD_SOURCE_(self%id_NO3, 0)
-         _ADD_SOURCE_(self%id_SiO4,0)
-         _ADD_SOURCE_(self%id_NH4, 0)
-         _ADD_SOURCE_(self%id_S1,  0)
-         _ADD_SOURCE_(self%id_S2,  0)
-         _ADD_SOURCE_(self%id_Z1,  0)
-         _ADD_SOURCE_(self%id_Z2,  0)
-         _ADD_SOURCE_(self%id_DN,  0)
-         _ADD_SOURCE_(self%id_DSi, 0)
-         _ADD_SOURCE_(self%id_PO4, 0)
-         _ADD_SOURCE_(self%id_DOX, 0)
-         _ADD_SOURCE_(self%id_CO2, 0)
-         _ADD_SOURCE_(self%id_ALK, 0)
+         !-----------------------------------------------------------------
+         _ADD_SOURCE_(self%id_NO3, qcos(1)/secs_pr_day)
+         _ADD_SOURCE_(self%id_SiO4,qcos(2)/secs_pr_day)
+         _ADD_SOURCE_(self%id_NH4, qcos(3)/secs_pr_day)
+         _ADD_SOURCE_(self%id_S1,  qcos(4)/secs_pr_day)
+         _ADD_SOURCE_(self%id_S2,  qcos(5)/secs_pr_day)
+         _ADD_SOURCE_(self%id_Z1,  qcos(6)/secs_pr_day)
+         _ADD_SOURCE_(self%id_Z2,  qcos(7)/secs_pr_day)
+         _ADD_SOURCE_(self%id_DN,  qcos(8)/secs_pr_day)
+         _ADD_SOURCE_(self%id_DSi, qcos(9)/secs_pr_day)
+         _ADD_SOURCE_(self%id_PO4, qcos(10)/secs_pr_day)
+         _ADD_SOURCE_(self%id_DOX, qcos(11)/secs_pr_day)
+         _ADD_SOURCE_(self%id_CO2, qcos(12)/secs_pr_day)
+         _ADD_SOURCE_(self%id_ALK, qcos(13)/secs_pr_day)
 
       _LOOP_END_
    end subroutine do
